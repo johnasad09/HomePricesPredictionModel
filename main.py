3645524a44eda1c65aa4ab4b94e499568747d08c
Asad Ullah <johnasad09@gmail.com>
2025-08-10 16:24:17 +0500

    Update main.py
    FINDING OPTIMAL LEAF NODES
    BEST FIT, REMOVING OVERFITTING AND UNDERFITTING

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
 
 melbourne_data = pd.read_csv('melb_data.csv')
 
@@ -49,6 +51,28 @@ melbourne_model.fit(train_X, train_y)
 
 # get predicted prices on validation data
 val_predictions = melbourne_model.predict(val_X)
+# MAE = 242567.53078055964
+
+
+# -----------------------COMPARING MODEL ACCURACY AND MAE SCORES WITH LEAF NODES-----------------------------#
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
 print(mean_absolute_error(val_y, val_predictions))
 
-# MAE = 242567.53078055964
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = min(scores, key=scores.get)
+
+# Fill in argument to make optimal size and uncomment
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=1)
+
+# fit the final model 
+final_model.fit(X, y)
